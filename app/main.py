72e742556102237e69c4ce46a7427593c51f0ec1
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-07-26 11:03:06 +0530

    Everyting in functions

--- app/main.py
+++ app/main.py
@@ -8,16 +8,18 @@ from RiskInDroid import RiskInDroid
 from model import db, Apk
 
 ALLOWED_EXTENSIONS = {"apk", "zip"}
-pp = pprint.PrettyPrinter(indent=4)
+
+pp = pprint.PrettyPrinter(indent=2)
+
 def check_if_valid_file_name(file_name):
     return (
         "." in file_name and file_name.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
     )
 
-file_path = "/home/anurag/dl/apktool/compass.apk"
-filename = "compass.apk"
-rid = RiskInDroid()
-permissions = rid.get_permission_json(file_path)
-risk = rid.calculate_risk(rid.get_feature_vector_from_json(permissions))
-pp.pprint(permissions)
-print("Risk Factor: ", risk)
+def get_risk(file_path):
+    file_path = "/home/anurag/dl/apktool/compass.apk"
+    rid = RiskInDroid()
+    permissions = rid.get_permission_json(file_path)
+    risk = rid.calculate_risk(rid.get_feature_vector_from_json(permissions))
+    permissions["risk_factor"] = risk
+    pp.pprint(permissions)
